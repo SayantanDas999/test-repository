3ce154f561c4cb17db061734bc0ff0b41d8f8050
Sayantan <sayantan.rkm@gmail.com>
2020-02-10 00:25:22 +0530

    Added a new User Resource after resolving SQLAlchemy register issue

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ Created on Sat Feb  1 00:49:13 2020
 @author: Sayantan
 """
 
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -15,11 +16,13 @@ from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"]=True
 app.secret_key = 'rivu'
 api = Api(app)
+
+db.init_app(app)
 
 @app.before_first_request
 def create_tables():
@@ -35,6 +38,5 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 
 if __name__=='__main__':
-    db.init_app(app)
     app.run()
     
