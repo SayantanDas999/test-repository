836d10a40325f016fb081b17c1db5cabdbb76765
Sayantan <sayantan.rkm@gmail.com>
2020-02-10 15:14:34 +0530

    Added JWT optional correcting the errors

--- resources/item.py
+++ resources/item.py
@@ -61,7 +61,7 @@ class ItemList(Resource):
             return {"itemlist":itemlist}
         else:
             for item in items:
-                itemlist.append(item.json()["name"])
+                itemlist.append(item.name)
             return {"itemlist":itemlist , "message":"More details available if you log in"}
     
         
